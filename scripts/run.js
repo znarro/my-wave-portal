5076d497ee917a221f01551d4634947a502eca6a
Zamir Narro <znarro@bsale.com.pe>
2021-12-05 15:55:42 -0500

    Manually call the functions in the contract

--- scripts/run.js
+++ scripts/run.js
@@ -1,10 +1,22 @@
 const main = async () => {
+  // fake wallet addresses
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   // compile our contract
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   // deploy to a new local Ethereum network
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
+
   console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
