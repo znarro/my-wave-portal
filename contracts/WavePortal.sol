ae352bafb4c69f94caaa19c97b363c1102202acf
Zamir Narro <znarro@bsale.com.pe>
2021-12-05 15:47:11 -0500

    Add functions to WavePortal

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,7 +7,23 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    // state variable, it's permanently stored in the blockchain
+    uint256 totalWaves;
+
+    // constructor executes once contract is officialy deployed
     constructor() {
         console.log("My first smart contract!"); 
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        // to call a smart contract fx you need to be connected with a valid wallet
+        // so msg.sender is the wallet address
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
